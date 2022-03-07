c24bd564e4be657fd92f6040116fe910743248a4
MERINO10617 <amerinos09@gmail.com>
2022-03-07 08:59:04 +0100

    Version 1 agregada variable

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -4,6 +4,7 @@ public class EjemploGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola git");
+		String v1 = "Alejandro merino0";
 	}
 
 }
